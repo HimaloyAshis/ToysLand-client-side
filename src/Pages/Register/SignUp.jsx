f3783ef57bbf72027081ce42e60c8904540be8e4
himaloyash <himaloyashis98@gmail.com>
2023-05-18 17:03:24 +0600

    image added

--- src/Pages/Register/SignUp.jsx
+++ src/Pages/Register/SignUp.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
+            <div className="hero-content flex-col lg:flex-row ">
                 <div className="text-center lg:text-left w-1/2">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
-                <div className="card flex-shrink-0 w-1/2 max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <div className="form-control">
                             <label className="label">
@@ -21,6 +21,9 @@ const Login = () => {
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
                         </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Login</button>
@@ -32,4 +35,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
