ddddaa1164bd1ee282e74cec7d1ba3d5cce272ce
himaloyash <himaloyashis98@gmail.com>
2023-05-18 17:09:27 +0600

    imgbb added

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <div className='bg p-5 lg:flex justify-between text-white'>
             <div className='flex'>
-                <img className='' src="" alt="" />
+                <img className='h-32' src="https://images.unsplash.com/photo-1551103782-8ab07afd45c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
                 <p className='text-2xl font-bold'>Toys Land</p>
             </div>
             <div className='lg:flex gap-3'>
