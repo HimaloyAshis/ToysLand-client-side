5cd1087724ae865900420bc9648a14a680ec4c04
himaloyash <himaloyashis98@gmail.com>
2023-05-19 23:31:13 +0600

    private route done

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext)
+    if(loading){
+        return <progress className="progress w-56"></progress>
+    }
+
+    if(user?.email){
+        return children
+    }
+    
+    return <Navigate to={'/login'}></Navigate>
 };
 
 export default PrivateRoute;
