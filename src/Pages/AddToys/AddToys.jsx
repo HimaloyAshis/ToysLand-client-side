c0f68e5fb55fedbdf57ffb9781e4823ce4e3920e
himaloyash <himaloyashis98@gmail.com>
2023-05-20 01:21:49 +0600

    good night

--- src/Pages/AddToys/AddToys.jsx
+++ src/Pages/AddToys/AddToys.jsx
@@ -42,9 +42,9 @@ const AddToys = () => {
                 <h2 className='text-center font-bold text-3xl py-6'>Add your Toys</h2>
                 <form onSubmit={handleSubmit(onSubmit)} className='gap-5 grid grid-cols-1 lg:grid-cols-2 w-2/3 mx-auto'>
 
-                    <input className='input' defaultValue="test" {...register("Url")} type='url' placeholder='Url' />
+                    <input className='input' defaultValue=" " {...register("Url")} type='url' placeholder='Url' />
 
-                    <input className='input' defaultValue="test" {...register("Toy_Name")} type='text' placeholder='Toy_Name' />
+                    <input className='input' defaultValue=" " {...register("Toy_Name")} type='text' placeholder='Toy_Name' />
 
                     <input className='input' defaultValue={user?.displayName} {...register("Seller_Name", { required: true })} placeholder='SellerName' type='text' />
                     <input className='input' defaultValue={user?.email} {...register("Seller_Email", { required: true })} placeholder='Seller_Email' type='email'/>
@@ -56,7 +56,7 @@ const AddToys = () => {
                     <input className='input' defaultValue={''} {...register("Price", { required: true })} placeholder='Price' type='number' />
                     <input className='input' defaultValue={''} {...register("Ratting", { required: true })} placeholder='Ratting' type='number'/>
                     <input className='input' defaultValue={''} {...register("Available_Quantity", { required: true })} placeholder='Available_Quantity' type='number'/>
-                    <textarea className='text p-2 h-20' defaultValue={''} {...register("description", { required: true })} placeholder='description' ></textarea>
+                    <textarea className='text p-2 h-20 rounded' defaultValue={''} {...register("description", { required: true })} placeholder='description' ></textarea>
 
                     {errors.exampleRequired && <span>This field is required</span>}
 
