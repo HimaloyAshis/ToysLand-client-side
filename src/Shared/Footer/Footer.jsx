b418e160c34b523cc2356ae317a49a8382d1354d
himaloyash <himaloyashis98@gmail.com>
2023-05-19 15:50:32 +0600

    service item done

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import './Footer.css'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='footBGC p-10 text-white'>
             <div>
-
+                <p>Subscribe Us</p>
+                <br />
+                <input type="email" className='h-10 p-2 rounded text-black' name="email" id="" />
+                <button className='btn ml-2'>Sign in</button>
             </div>
             <div>
-
+                <h2>Service Items</h2>
+                <Link>Toys</Link>
+                <Link>All Product</Link>
             </div>
             <div>
                 
