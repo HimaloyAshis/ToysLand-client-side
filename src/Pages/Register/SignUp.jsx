8b136e34c9ed3775392a8d4441804a1f4e6845e6
himaloyash <himaloyashis98@gmail.com>
2023-05-19 23:21:24 +0600

    signUp added

--- src/Pages/Register/SignUp.jsx
+++ src/Pages/Register/SignUp.jsx
@@ -1,33 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import './SignUp.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const SignUp = () => {
+
+    const {createUser} = useContext(AuthContext)
+
+    const handleSignUp = (event) => {
+        event.preventDefault()
+        const form = event.target
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value
+        const photo = form.photo.value
+        console.log(email, password, name, photo)
+
+        createUser(email, password)
+        .then(result=>{
+            const loggedUser = result.user
+            console.log(loggedUser)
+        })
+        .catch(error=>console.log(error.message))
+
+        
+    }
+
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row ">
-                <div className="text-center lg:text-left w-1/2">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                </div>
+        <div className="hero min-h-screen log ">
+            <div className="hero-content flex-col lg:flex-row">
+                
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
+                    <div className="card-body login rounded ">
+                        <h1 className="text-5xl font-bold text-white">Sign Up now!</h1>
+                        <form onSubmit={handleSignUp}>
+                            <div className="form-control">
+                                <label className="label ">
+                                    <span className="label-text text-white">Name</span>
+                                </label>
+                                <input type="text" name='name' required placeholder="name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label ">
+                                    <span className="label-text text-white">Email</span>
+                                </label>
+                                <input type="email" name='email' required placeholder="email" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-white">Password</span>
+                                </label>
+                                <input type="password" name='password' required placeholder="password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label ">
+                                    <span className="label-text text-white">Photo</span>
+                                </label>
+                                <input type="URL" name='photo' required placeholder="URL" className="input input-bordered" />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn text-white">Sign Up</button>
+                            </div>
+                        </form>
+                        <p className='text-white'>Already have an account ? please <Link to={'/login'} className='text-orange-500'>SignIn</Link> </p>
                     </div>
                 </div>
             </div>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 export const AuthContext = createContext(null)
 
@@ -9,14 +9,45 @@ const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider()
 
     const createUser =( email, password)=>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    const loginUser = (email, password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const gProvider =()=>{
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    useEffect(()=>{
+       const unSubscribe = onAuthStateChanged(auth, currentUser=>{
+            console.log('current user' ,currentUser)
+            setLoading(false)
+        })
+        return ()=>{
+            return unSubscribe()
+        }
+    },[])
+
+    const userInfo ={
+        user,
+        loading,
+        createUser,
+        loginUser,
+        gProvider,
+
+    }
 
     return (
-        <AuthContext.Provider value={''}>
+        <AuthContext.Provider value={userInfo}>
             {children}
         </AuthContext.Provider>
     );
