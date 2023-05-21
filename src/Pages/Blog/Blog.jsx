52df5ff8e86512d6bcdfe64f3566735ae593deed
himaloyash <himaloyashis98@gmail.com>
2023-05-21 13:55:28 +0600

    question added

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -3,8 +3,13 @@ import './Blog.css'
 
 const Blog = () => {
     return (
-        <div className=''>
-            <h2 className='text-4xl font-bold'>Question and answer part</h2>
+        <div className='p-10'>
+            <h2 className='text-4xl font-bold text-center py-5'>Question and answer part</h2>
+            <div className=''>
+                <h1 className='text-2xl font-semibold'>What is an access token and refresh token? How do they work and where should we store them on the client-side?</h1>
+                <br />
+                <p></p>
+            </div>
         </div>
     );
 };
