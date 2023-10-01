757e457ea814a49d2562baefdfa80a84ff06b3c6
himaloyash <himaloyashis98@gmail.com>
2023-10-02 00:52:08 +0600

    gallery centered

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = () => {
                 <h2 className='text-4xl mt-6 text-white font-bold'>ToysLand Gallery</h2>
                 <hr className='border-2 bg-slate-200 w-1/3 m-auto mb-3 mt-3' />
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-3 w-full m-auto sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-10'>
+            <div className='grid grid-cols-1 md:grid-cols-3 justify-center items-center w-10/12 m-auto sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-10'>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA0UP3wTwX5rkTC16bYTw2UECeJ0VFFsrRbQ&usqp=CAU" alt="" />
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA0UP3wTwX5rkTC16bYTw2UECeJ0VFFsrRbQ&usqp=CAU" alt="" />
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq-WvjCODO6okIZtu-tM3M9EoXZLG0eIdRHw&usqp=CAU" alt="" />
