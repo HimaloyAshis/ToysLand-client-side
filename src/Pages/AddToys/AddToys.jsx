9f9d07acb36a64688e72d3d0992c2f5b89a88ff9
himaloyash <himaloyashis98@gmail.com>
2023-10-02 00:34:40 +0600

    I think all ok

--- src/Pages/AddToys/AddToys.jsx
+++ src/Pages/AddToys/AddToys.jsx
@@ -47,24 +47,26 @@ const AddToys = () => {
     return (
         <>
             <div className='p-20 input-bg'>
-                <h2 className='text-center text-[#f8f7f7] font-bold text-3xl py-6'>Add your Toys</h2>
+                <h2 className='text-center text-[#f8f7f7] font-bold text-3xl pt-6'>Add your Toys</h2>
+                <hr className='border-2   w-2/4 m-auto mb-8 mt-3' />
+
                 <form onSubmit={handleSubmit(onSubmit)} className='gap-5 grid grid-cols-1 lg:grid-cols-2 w-2/3 mx-auto'>
 
-                    <input className='input' defaultValue=" " {...register("Url")} type='url' placeholder='Url' />
+                    <input className='input text-black' defaultValue=" " {...register("Url")} type='url' placeholder='Url' />
 
-                    <input className='input' defaultValue="moana" {...register("Toy_Name")} type='text' placeholder='Name' />
+                    <input className='input text-black' defaultValue="moana" {...register("Toy_Name")} type='text' placeholder='Name' />
 
-                    <input className='input' defaultValue={user?.displayName} {...register("Seller_Name", { required: true })} placeholder='SellerName' type='text' />
-                    <input className='input' defaultValue={user?.email} {...register("Seller_Email", { required: true })} placeholder='Seller_Email' type='email'/>
-                    <select className='input' {...register("SubCategory")}>
+                    <input className='input text-black' defaultValue={user?.displayName} {...register("Seller_Name", { required: true })} placeholder='SellerName' type='text' />
+                    <input className='input text-black' defaultValue={user?.email} {...register("Seller_Email", { required: true })} placeholder='Seller_Email' type='email'/>
+                    <select className='input text-black' {...register("SubCategory")}>
                         <option value="Animated_character">Animated character</option>
                         <option value="Disney_princess">Disney princess</option>
                         <option value="Frozen_dolls">Frozen dolls</option>
                     </select>
-                    <input className='input' defaultValue={''} {...register("Price", { required: true })} placeholder='Price' type='number' />
-                    <input className='input' defaultValue={''} {...register("Ratting", { required: true })} placeholder='Ratting' type='text'/>
-                    <input className='input' defaultValue={''} {...register("Available_Quantity", { required: true })} placeholder='Available_Quantity' type='number'/>
-                    <textarea className='text p-2 h-20 rounded' defaultValue={''} {...register("description", { required: true })} placeholder='description' ></textarea>
+                    <input className='input text-black' defaultValue={''} {...register("Price", { required: true })} placeholder='Price' type='number' />
+                    <input className='input text-black' defaultValue={''} {...register("Ratting", { required: true })} placeholder='Ratting' type='text'/>
+                    <input className='input text-black' defaultValue={''} {...register("Available_Quantity", { required: true })} placeholder='Available_Quantity' type='number'/>
+                    <textarea className='text text-black p-2 h-20 rounded' defaultValue={''} {...register("description", { required: true })} placeholder='description' ></textarea>
 
                     {errors.exampleRequired && <span>This field is required</span>}
 
--- src/Pages/UpdateToys/UpdateToys.jsx
+++ src/Pages/UpdateToys/UpdateToys.jsx
@@ -55,25 +55,25 @@ const UpdateToys = () => {
 
                 <form onSubmit={handleSubmit(onSubmit)} className='gap-5 grid grid-cols-1 lg:grid-cols-2 w-2/3 mx-auto'>
 
-                    <input className='input' defaultValue={Url} {...register("Url")} type='url' placeholder='Url' />
+                    <input className='input text-black' defaultValue={Url} {...register("Url")} type='url' placeholder='Url' />
 
-                    <input className='input' defaultValue={Toy_Name} {...register("Toy_Name")} type='text' placeholder='Name' />
+                    <input className='input text-black' defaultValue={Toy_Name} {...register("Toy_Name")} type='text' placeholder='Name' />
 
-                    <input className='input' defaultValue={user?.displayName} {...register("Seller_Name", { required: true })} placeholder='SellerName' type='text' />
-                    <input className='input' defaultValue={user?.email} {...register("Seller_Email", { required: true })} placeholder='Seller_Email' type='email' />
-                    <select className='input' {...register("SubCategory")}>
+                    <input className='input text-black' defaultValue={user?.displayName} {...register("Seller_Name", { required: true })} placeholder='SellerName' type='text' />
+                    <input className='input text-black' defaultValue={user?.email} {...register("Seller_Email", { required: true })} placeholder='Seller_Email' type='email' />
+                    <select className='input text-black' {...register("SubCategory")}>
                         <option value="Animated_character">Animated character</option>
                         <option value="Disney_princess">Disney princess</option>
                         <option value="Frozen_dolls">Frozen dolls</option>
                     </select>
-                    <input className='input' defaultValue={ Price} {...register("Price", { required: true })} placeholder='Price' type='number' />
-                    <input className='input' defaultValue={Ratting} {...register("Ratting", { required: true })} placeholder='Ratting' type='text' />
-                    <input className='input' defaultValue={Available_Quantity} {...register("Available_Quantity", { required: true })} placeholder='Available_Quantity' type='number' />
-                    <textarea className='text p-2 h-20 rounded' defaultValue={description} {...register("description", { required: true })} placeholder='description' ></textarea>
+                    <input className='input text-black' defaultValue={ Price} {...register("Price", { required: true })} placeholder='Price' type='number' />
+                    <input className='input text-black' defaultValue={Ratting} {...register("Ratting", { required: true })} placeholder='Ratting' type='text' />
+                    <input className='input text-black' defaultValue={Available_Quantity} {...register("Available_Quantity", { required: true })} placeholder='Available_Quantity' type='number' />
+                    <textarea className='text p-2 h-20 rounded text-black' defaultValue={description} {...register("description", { required: true })} placeholder='description' ></textarea>
 
                     {errors.exampleRequired && <span>This field is required</span>}
 
-                    <input className='btn w-full mx-auto' type="submit" />
+                    <input className='btn w-full  mx-auto' type="submit" />
                 </form>
             </div>
         </>
