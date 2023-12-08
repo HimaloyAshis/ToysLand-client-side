6dd3fda8c09e858b0c47fdff87c109a5413e2b53
himaloyash <himaloyashis98@gmail.com>
2023-12-08 16:19:15 +0600

    responsive navbar added

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,12 +1,15 @@
-import React, { useContext } from 'react';
-import { FaBeer, FaStar } from 'react-icons/fa';
+import React, { useContext, useState } from 'react';
+import { FaStar } from 'react-icons/fa';
 import './Navbar.css'
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Provider/AuthProvider';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid'
 
 const NavBar = () => {
+
+    const [Open, setOpen] = useState(false)
 
     const { user, LogOut } = useContext(AuthContext)
     console.log(user)
@@ -21,23 +24,34 @@ const NavBar = () => {
     }
 
     return (
-        <div className='bg p-5 lg:flex justify-between items-center text-white'>
-            <div className='flex gap-4'>
-                <img className='h-14' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_IMm90lBVuwXW4YXgYc8yM3n6HgrblB5Ffw&usqp=CAU" alt="" />
-                <p className='text-2xl font-bold inline-flex items-center'>T<FaStar className='text-orange-500'></FaStar> ysLand</p>
+        <div>
+            <div>
+                <span onClick={() => setOpen(!Open)} className='md:hidden '>
+                    {
+                        Open === true ? < XMarkIcon className="h-6 w-6 text-blue-500 cursor-wait" />
+                            : <Bars3Icon className="h-6 w-6 text-blue-500 cursor-pointer" />
+                    }
+                </span>
             </div>
-            <div className='lg:flex gap-3'>
-                <ToastContainer></ToastContainer>
-                <Link className='butn'>Home</Link>
-                <Link to={'/blog'} className='butn'>Blog</Link>
-                <Link to={'/allToys'} className='butn'>All toys</Link>
-                {user ? <>
-                    <Link to={'/myToys'} className='butn'>My toys</Link>
-                    <Link to={'/addToys'} className='butn'>Add toys</Link>
-                    <button onClick={handleLogOut} className='butn'>LogOut</button>
-                    <img className='rounded-full h-8' src={user?.photoURL} title={user?.displayName} alt="" />
-                </>
-                    : <Link to={'/login'} className='butn'>Login</Link>}
+            <div className='bg p-5 lg:flex justify-between items-center text-white'>
+
+                <div className='flex gap-4'>
+                    <p className='text-2xl font-bold inline-flex items-center'>T<FaStar className='text-orange-500'></FaStar> ysLand</p>
+                </div>
+
+                <div className='lg:flex gap-3'>
+                    <ToastContainer></ToastContainer>
+                    <Link className='butn'>Home</Link>
+                    <Link to={'/blog'} className='butn'>Blog</Link>
+                    <Link to={'/allToys'} className='butn'>All toys</Link>
+                    {user ? <>
+                        <Link to={'/myToys'} className='butn'>My toys</Link>
+                        <Link to={'/addToys'} className='butn'>Add toys</Link>
+                        <button onClick={handleLogOut} className='butn'>LogOut</button>
+                        <img className='rounded-full h-8' src={user?.photoURL} title={user?.displayName} alt="" />
+                    </>
+                        : <Link to={'/login'} className='butn'>Login</Link>}
+                </div>
             </div>
         </div>
     );
